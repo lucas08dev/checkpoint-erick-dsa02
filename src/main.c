342a406097f029d366d920f019bfc9cb34885bdf
nicolascnishi <nicolas.c.nishi@gmail.com>
2026-04-24 09:46:05 -0300

    Adicionei o printf faltando e o break para finalizar o primeiro caso

--- src/main.c
+++ src/main.c
@@ -21,6 +21,8 @@ int main() {
                 scanf("%f %f %f", &n1, &n2, &n3);
 
                 media = (n1 + n2 + n3) / 3;
+                printf("Media = %.2f\n", media);
+                break;
             }
         }
     }
