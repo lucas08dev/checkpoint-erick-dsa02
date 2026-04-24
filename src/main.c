ba5cb1193c3840de459d03698e5c733a5840105e
reicheguilherme-spec <reicheguilherme@gmail.com>
2026-04-24 09:28:09 -0300

    Adicionei o while e os prints, fazendo com que o programa tenha uma interface de menu, assim o usuário pode escolher qual opcao ele deseja.

--- src/main.c
+++ src/main.c
@@ -2,6 +2,16 @@
 
 int main() {
     int opcao = -1;
+    
+    while (opcao != 0) {
+        printf("\n--- MENU ---\n");
+        printf("1 - Calcular media\n");
+        printf("2 - Classificar aluno\n");
+        printf("3 - Derivada (ax^2 + bx + c)\n");
+        printf("0 - Sair\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+    
 
     
     return 0;
